8d9065d3ec1e133135310ae8b05515b75365bec8
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2025-03-23 20:38:31 -0400

    added getPolls api

--- src/poll/Poll.dto.ts
+++ src/poll/Poll.dto.ts
@@ -2,9 +2,12 @@ import {
   IsArray,
   IsBoolean,
   IsDateString,
+  IsEnum,
+  IsInt,
   IsNotEmpty,
   IsOptional,
   IsString,
+  Min,
 } from 'class-validator';
 
 export class CreatePollDto {
@@ -37,4 +40,36 @@ export class CreatePollDto {
   @IsBoolean()
   @IsOptional()
   isAnonymous?: boolean;
+}
+
+export class GetPollsDto {
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  limit?: number = 10;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  userVoted?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  userCreated?: boolean;
+
+  @IsOptional()
+  @IsEnum(['endDate', 'participantCount', 'creationDate'])
+  sortBy?: 'endDate' | 'participantCount' | 'creationDate';
+
+  @IsOptional()
+  @IsEnum(['asc', 'desc'])
+  sortOrder?: 'asc' | 'desc';
 }
