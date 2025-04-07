8788a13f9e217bf58f080c627b772e17e121d3c4
Ramin <raminramazanpour@gmail.com>
2025-04-08 03:04:35 +0330

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseModule } from './database/database.module';
 import { PollModule } from './poll/poll.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule, PollModule, AuthModule],
+  imports: [DatabaseModule, PollModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService, AuthService],
 })
