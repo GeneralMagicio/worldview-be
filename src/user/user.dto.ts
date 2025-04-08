dabc453691d0838ec7e1c73b71f87450a76413c6
Ramin <raminramazanpour@gmail.com>
2025-04-08 18:24:27 +0330

    add worldProfilePic

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -6,6 +6,7 @@ export class UserDataResponseDto {
   pollsCreated: number;
   pollsParticipated: number;
   worldID: string;
+  worldProfilePic?: string | null;
 }
 
 export class GetUserActivitiesDto {
