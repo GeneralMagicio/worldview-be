f7e44202e2cf8715b5d24759719356a5f7e39037
Ramin <raminramazanpour@gmail.com>
2025-04-14 16:49:54 +0330

    add IsPositiveInteger and IsRecordStringNumber

---
+++ src/common/validators.ts
@@ -0,0 +1,33 @@
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'isNonNegativeInteger', async: false })
+export class IsPositiveInteger implements ValidatorConstraintInterface {
+  validate(value: any): boolean {
+    if (typeof value !== 'number' && typeof value !== 'string') {
+      return false;
+    }
+    const num = Number(value);
+    return Number.isInteger(num) && num > 0;
+  }
+  defaultMessage(): string {
+    return '$property must be a positive integer';
+  }
+}
+
+@ValidatorConstraint({ name: 'IsRecordStringNumber', async: false })
+export class IsRecordStringNumber implements ValidatorConstraintInterface {
+  validate(value: any): boolean {
+    if (typeof value !== 'object' || value === null || Array.isArray(value)) {
+      return false;
+    }
+    return Object.entries(value).every(
+      ([key, val]) => typeof key === 'string' && typeof val === 'number',
+    );
+  }
+  defaultMessage(): string {
+    return 'weightDistribution must be a record with string keys and number values';
+  }
+}
