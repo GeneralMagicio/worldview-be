63b414c0eaa27cc478d81de0e5e43de34bdb7895
Ramin <raminramazanpour@gmail.com>
2025-04-18 02:40:38 +0330

    add UserService to poll module

--- src/poll/poll.module.ts
+++ src/poll/poll.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PollService } from './poll.service';
 import { PollController } from './poll.controller';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   controllers: [PollController],
-  providers: [PollService],
+  providers: [PollService, UserService],
 })
 export class PollModule {}
