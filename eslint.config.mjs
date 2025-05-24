57d8c4da397990cb0e1846cf19831ea4d1ad1ed3
Ramin <raminramazanpour@gmail.com>
2025-05-24 14:30:15 +0330

    add no-explicit-any no-floating-promises no-unsafe-argument to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import eslint from '@eslint/js';
 import * as eslintPluginImport from 'eslint-plugin-import';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
@@ -31,9 +30,6 @@ export default tseslint.config(
       import: eslintPluginImport,
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
       'prettier/prettier': [
         'error',
         {
